bccbedc4ee4ef85abe800e01dc9bbdab519021bb
lewis-k <lewis-k@users.noreply.github.com>
2015-11-27 20:11:15 -0700

    Isolate the theater mode from the global namespace and eliminate a constant from the source.

--- theater-mode.js
+++ theater-mode.js
@@ -1,49 +1,52 @@
-$("#theater-mode").find("input").on("click", toggleTheaterMode);
+(function(){
+	var originalTlkioHeight = $("#tlkio").height();
+	$("#theater-mode").find("input").on("click", toggleTheaterMode);
 
-function styleTheaterMode()
-{
-	$("#sidebar").hide();
-	$("#content").parent().add("#video-content, #content, #user-content, #tab_bar").addClass("theater");
-
-	function dynamicStyling()
+	function styleTheaterMode()
 	{
-		/* Set width to nest user content alongside the movie. */
-		$("#user-content").width(function() {
-			return	$("#content").width()
-				- $("div.link:visible").outerWidth()
-				- parseInt($("div.link:visible").css('margin-left'), 10);
-		});
+		$("#sidebar").hide();
+		$("#content").parent().add("#video-content, #content, #user-content, #tab_bar").addClass("theater");
 
-		/* Chat scrolls down, so align bottom of chat with bottom of the movie block. */
-		$("#tlkio").height(function(){
-			return 	$("#video-content").outerHeight()
-				- $("#posts").position().top /* Convoluted because to set height properly, all elements in the calculation must be visible. So no self-referencing. */
-				- 2*parseInt($("#posts").css('padding-top'), 10);
-				- 2*parseInt($("#posts").css('border-top'), 10);
-		});
+		function dynamicStyling()
+		{
+			/* Set width to nest user content alongside the movie. */
+			$("#user-content").width(function() {
+				return	$("#content").width()
+					- $("div.link:visible").outerWidth()
+					- parseInt($("div.link:visible").css('margin-left'), 10);
+			});
+
+			/* Chat scrolls down, so align bottom of chat with bottom of the movie block. */
+			$("#tlkio").height(function(){
+				return 	$("#video-content").outerHeight()
+					- $("#posts").position().top /* Convoluted because to set height properly, all elements in the calculation must be visible. So no self-referencing. */
+					- 2*parseInt($("#posts").css('padding-top'), 10);
+					- 2*parseInt($("#posts").css('border-top'), 10);
+			});
+		}
+
+		dynamicStyling();
+		$(window).on("resize.mst3k", dynamicStyling);
 	}
 
-	dynamicStyling();
-	$(window).on("resize.mst3k", dynamicStyling);
-}
-
-function unstyleTheaterMode()
-{
-	$(window).off("resize.mst3k");
-	$("#content").parent().add("#video-content, #content, #user-content, #tab_bar").removeClass("theater");
-	$("#user-content").width("auto");
-	$("#tlkio").height('1000px'); /* Constant from source */
-	$("#sidebar").show();
-}
-
-function toggleTheaterMode(event)
-{
-	if (event.target.checked)
+	function unstyleTheaterMode()
 	{
-		styleTheaterMode();
+		$(window).off("resize.mst3k");
+		$("#content").parent().add("#video-content, #content, #user-content, #tab_bar").removeClass("theater");
+		$("#user-content").width("auto");
+		$("#tlkio").height(originalTlkioHeight + 'px');
+		$("#sidebar").show();
 	}
-	else
+
+	function toggleTheaterMode(event)
 	{
-		unstyleTheaterMode();
+		if (event.target.checked)
+		{
+			styleTheaterMode();
+		}
+		else
+		{
+			unstyleTheaterMode();
+		}
 	}
-}
+})();
